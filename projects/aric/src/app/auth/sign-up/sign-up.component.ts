a3ce8c731ce9ce166ca545aa7f09e79a1d91bf27
RicardoVessaro <ricardolvessaro@gmail.com>
2023-11-15 12:20:56 -0300

    Member Login#3 - Register page finished

--- projects/aric/src/app/auth/sign-up/sign-up.component.ts
+++ projects/aric/src/app/auth/sign-up/sign-up.component.ts
@@ -1,14 +1,19 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+
 import { MatInputModule } from '@angular/material/input'
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule, NgForm } from '@angular/forms';
-import { Store } from '@ngrx/store';
-import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { signUp } from '../store/auth.actions';
 
+type err = {
+  [s: string]: boolean;
+};
 
 @Component({
   selector: 'app-sign-up',
@@ -18,23 +23,85 @@ import { signUp } from '../store/auth.actions';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    FormsModule,
-    MatSnackBarModule
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatIconModule
   ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
-export class SignUpComponent {
-  @ViewChild('f') signupForm!: NgForm;
+export class SignUpComponent implements OnInit {
+  signupForm!: FormGroup;
+  hidePassword = true;
+  hideConfirmPassword = true;
 
   constructor(
     private store: Store
   ) {}
 
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'username': new FormControl('', Validators.required),
+      'name': new FormControl('', Validators.required),
+      'email': new FormControl('', [Validators.required, Validators.email]),
+      'password': new FormControl('', [Validators.required, this.passwordPattern]),
+      'confirmPassword': new FormControl('', [Validators.required, this.passwordMatch.bind(this)]),
+    });
+  }
+
   onSubmit() {
     console.log('onSubmit');
     console.log(this.signupForm.value);
     this.store.dispatch(signUp(this.signupForm.value));
+  }
+
+  passwordMatch(control: FormControl) : err {
+    if(this.signupForm 
+        && control.value !== this.signupForm.get('password')!.value) {
+      
+      return {'passwordsMustMatch' : true};
+    }
+
+    return null as any;
+  }
+
+  passwordPattern(control: FormControl) : err {
+    let error : err = {};
+    let matches = false;
+
+    if(!new RegExp('([a-z])').test(control.value)) {
+      matches = true;
+      error = { ...error, 'lowercase': true } 
+    }
+
+    if(!new RegExp('([A-Z])').test(control.value)) {
+      matches = true;
+      error = { ...error, 'uppercase': true } 
+    }
+
+    if(!new RegExp('([\\d])').test(control.value)) {
+      matches = true;
+      error = { ...error, 'number': true } 
+    }
+
+    if(!new RegExp('([@$!%*?&])').test(control.value)) {
+      matches = true;
+      error = { ...error, 'specialCharacter': true } 
+    }
+
+    if (matches) {
+      return error;
+    } 
+
+    return null as any;
+  }
+
+  formHasError(formControlName:string, errorCode:string) {
+    const control = this.signupForm.get(formControlName);
+
+    return control 
+      && control.hasError(errorCode)
+      && control.touched;
   }
 
 }
